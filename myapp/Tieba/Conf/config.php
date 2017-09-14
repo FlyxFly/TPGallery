8a2ed070f8cf6b922a292aab3b112789e54124c8
小明 <kiwi1220@gmail.com>
2017-09-14 09:01:55 +0800

    Add tieba photo module

---
+++ myapp/Tieba/Conf/config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+'DB_NAME'   => 'tieba',// 数据库名
+'img_url_prefix' => 'https://gg.d-hl.com/tieba/',
+'index_item_per_page'=>21,
+'search_type'=>['userName','userId','content']
+);
